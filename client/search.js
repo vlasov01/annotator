bcc0a7e7dfc1a44ed7636fac1a2953d49522a519
Joel Chan <chozen86@gmail.com>
2015-08-15 12:20:40 -0400

    initial working commit of search interface - successful full-text search and highlighting of matching keywords in documents

---
+++ client/search.js
@@ -0,0 +1,119 @@
+var logger = new Logger('Client:search');
+
+Logger.setLevel('Client:search', 'trace');
+// Logger.setLevel('Client:search', 'debug');
+// Logger.setLevel('Client:search', 'info');
+// Logger.setLevel('Client:search', 'warn');
+
+var resultLength = 0;
+var options = {
+    keepHistory: 1000 * 60 * 5,
+    localSearch: true
+};
+var fields = ['content'];
+DocSearch = new SearchSource('documents', fields, options);
+
+Template.SeedDocument.helpers({
+    sentences: function() {
+        logger.debug("Getting sentences...");
+        return Sentences.find({docID: Session.get("currentDoc")._id},
+                                {sort: {psn: 1}});
+    }
+});
+
+Template.SearchBar.events({
+    'click .search-apply-btn' : function(){
+        var query = $('#search-query').val(); // grab query from text form
+        Session.set("searchQuery",query);
+        DocSearch.search(query);
+        $('.search-apply-btn').addClass('btn-success');
+        // logger.trace("Created new query: " + Session.get("searchQuery"));
+    },
+
+    'keyup input' : function(e, target){
+        // logger.debug(e);
+        // logger.debug(target);
+        if(e.keyCode===13) {
+          var btn = $('.search-apply-btn')
+          btn.click();
+        }
+    },
+
+    // clear full-text search of idea content
+    'click .search-remove-btn' : function(){
+        Session.set("searchQuery","############################");
+        DocSearch.search("############################");
+        $('.search-apply-btn').removeClass('btn-success');
+        $('#search-query').val("");
+        $('.doc-match').unhighlight();
+    },
+})
+
+Template.SearchResults.rendered = function () {
+    DocSearch.search("############################");
+};
+
+Template.SearchResults.helpers({
+    matchingDocs: function() {
+        return DocSearch.getData({
+              transform: function(matchText, regExp) {
+                return matchText.replace(regExp, "<b>$&</b>")
+              },
+              sort: {isoScore: -1}
+            });
+        // var query = Session.get("searchQuery");
+        // logger.trace("New query created: " + query);
+
+        // // allDocs = Documents.find().fetch();
+        // // logger.debug("Searching through " + allDocs.length + " documents");
+
+        // var queriedDocs = [];
+        // if (query != "") {
+        //     queryArr = stringToWords(query);
+        //     Documents.find().forEach(function(doc){
+        //         if (searchQueryMatch(doc,queryArr)) {
+        //             queriedDocs.push(doc);
+        //         } else {
+        //         }
+        //     });
+            
+        // } 
+        // resultLength = queriedDocs.length;
+        // logger.trace("Found " + resultLength + " matches");
+        // logger.trace("Matching documents: " + JSON.stringify(queriedDocs));
+        // return queriedDocs;
+    },
+    hasMatches: function() {
+        resultLength = DocSearch.getData({
+              transform: function(matchText, regExp) {
+                return matchText.replace(regExp, "<b>$&</b>")
+              },
+              sort: {isoScore: -1}
+            }).length;
+        if (resultLength < 1) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+    numMatches: function() {
+        return DocSearch.getData({
+              transform: function(matchText, regExp) {
+                return matchText.replace(regExp, "<b>$&</b>")
+              },
+              sort: {isoScore: -1}
+            }).length;
+    }
+});
+
+Template.Document.rendered = function() {
+    $('.doc-match').unhighlight();
+    var query = Session.get("searchQuery");
+    $('.doc-match').highlight(query.split(" "));
+};
+
+Template.Document.helpers({
+    sentences: function() {
+        return Sentences.find({docID: this._id}, {sort: {psn: 1}});
+    }
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -52,4 +52,26 @@ if (Documents.find().count() == 0) {
   // index for hopefully faster performance!
   Sentences._ensureIndex({docID: 1});
   Words._ensureIndex({sentenceID: 1});
+
+}
+
+SearchSource.defineSource('documents', function(searchText, options) {
+  var options = {sort: {isoScore: -1}, limit: 20};
+
+  if (searchText) {
+    var regExp = buildRegExp(searchText);
+    var selector = {content: regExp};
+    return Documents.find(selector, options).fetch();
+  } else {
+    return Documents.find({}, options).fetch();
+  }
+});
+
+function buildRegExp(searchText) {
+  var words = searchText.trim().split(/[ \-\:]+/);
+  var exps = _.map(words, function(word) {
+    return "(?=.*" + word + ")";
+  });
+  var fullExp = exps.join('') + ".+";
+  return new RegExp(fullExp, "i");
 }
