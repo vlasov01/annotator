3098e3fc775e8e032fb986f8241b18d9e72214fb
Joel Chan <chozen86@gmail.com>
2016-08-25 21:20:47 -0400

    enable random sampling of doc if didn't enter docID - just for now - remember to change when we collect data again

--- client/land.js
+++ client/land.js
@@ -9,21 +9,28 @@ Template.land.events({
     'click .continue' : function() {
         var userName = $('#userName').val();
         var docTitle = $('#docTitle').val();
-        if (userName == "" || docTitle == "") {
-            logger.warn("User is not logged in");
-            alert("You need to have entered both your MTurkID and the docID from the HIT to continue");
+        if (docTitle == "") {
+          var newDoc = getRandomElement(Documents.find().fetch());
+          docTitle = newDoc.fileName.replace(".txt", "");
+        }
+        // if (userName == "" || docTitle == "") {
+        //     logger.warn("User is not logged in");
+        //     alert("You need to have entered both your MTurkID and the docID from the HIT to continue");
+        if (userName == "") {
+          logger.warn("User is not logged in");
+          alert("Please enter a userName - anything will do. Just be sure to remember it if you want to use it again.");
         } else {
             logger.trace("User " + userName + " clicked continue");
             userID = UserManager.loginUser(userName);
-            Router.go("SearchInstructions", {userID: userID, docTitle: docTitle});    
+            Router.go("SearchInstructions", {userID: userID, docTitle: docTitle});
         }
         // Router.go("Tutorial", {userID: userID});
         // if (Meteor.user()) {
         //     logger.trace("Sending user to tutorial");
-        //     Router.go("Tutorial");    
+        //     Router.go("Tutorial");
         // } else {
         //     alert("Please sign in or create an account (in the menu in the top right-hand corner of the page) before continuing");
-        // }   
+        // }
     },
     'keyup input#userName': function (evt) {
       if(evt.keyCode==13) {
