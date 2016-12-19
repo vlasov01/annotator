ab9541aa5f958e0de0d2ce0f526e924d5b83f977
Sergey Vlasov <sergey_vlasov@yahoo.com>
2016-12-18 21:44:43 -0500

    Added missing files

---
+++ server/methods/read/documents.js
@@ -0,0 +1,38 @@
+var logger = new Logger('Documents:read');
+// Comment out to use global logging level
+Logger.setLevel('Documents:read', 'trace');
+
+Meteor.methods({
+  getSample( filter ) {
+	//logger.debug("Documents sampling for user: "+filter);
+	//db.getCollection('documents').aggregate({ $sample: { size: 1 } }).result[0]._id
+
+    let UserID = filter;
+	let hasAnnotated = true;
+
+	do {
+		let result = Documents.aggregate({ $sample: { size: 1 } });
+		var sampledDoc = result[0];
+		if (!isInList(UserID, sampledDoc.annotatedBy)) {
+		 hasAnnotated = false;
+		}
+	} while (hasAnnotated);
+	
+	//logger.debug("Explain documents:", JSON.stringify(result), null, 2);
+	//logger.debug("Explain documents:"+sampledDoc._id);
+	return sampledDoc;
+  },
+  getSentences( filter ) {
+	//logger.debug("Sentences for docID: "+filter);
+	//db.getCollection('sentences').find({docID: "XsuaJpy6JCgmpAQjy"}).sort({psn: 1})
+	let sentences = Sentences.find({docID: filter}, { sort: {psn: 1} }).fetch();
+	//logger.debug("Explain documents:"+sentences[0].content);
+	return sentences;
+  },
+  getWords( filter ) {
+	//logger.debug("Words for sentence: "+filter);
+	let words = Words.find({docID: filter},{sort: { sequence : 1 }}).fetch();
+	//logger.debug("Explain words:"+words[0].content);
+	return words;
+  }
+});
